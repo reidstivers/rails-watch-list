69f076f691e999f253e796f5c4429fd7dd8063f8
Reid Stivers <reidstivers@gmail.com>
2024-07-06 12:03:26 +0900

    all validations done

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie, presence: true, uniqueness: { scope: :list_id }
+  validates :comment, length: { minimum: 6 }
+end
